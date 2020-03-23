e26a977a46fa885261cb3eb33ca41da68bf22bfa
GIT18195644 <it18195644@my.sliit.lk>
2020-03-24 01:20:19 +0530

    Size, Method, Variable Created Successfully

--- CDE_IT/src/ValueGenerator/MethodData.java
+++ CDE_IT/src/ValueGenerator/MethodData.java
@@ -12,6 +12,8 @@ public class MethodData {
 	int Ncdtp;
 	int Cm;
 	
+	int sum, WMRTM, NCDTPM, NPDTPM;
+	
 	String token1 ="";
 	List<Integer> ValueList = new ArrayList<Integer>();
 	MethodReader methodreader = new MethodReader();
@@ -50,7 +52,7 @@ public class MethodData {
 	//WMRT Done
 	public int FindWmrt(String CodeLine) {
 		int Li_Count = 0;
-		String token1 = "";
+		
 		Scanner scanner = new Scanner(CodeLine);
 		while (scanner.hasNext()) {
 			token1 = scanner.next();
@@ -74,30 +76,80 @@ public class MethodData {
 			}
 			
 			Li_Count = 0;
+			WMRTM = Li_Count;
 			return 0;
 			
 					
 		}
-		scanner.close();	
+		scanner.close();
+		WMRTM = Li_Count;
 		return Li_Count;
 
 	}
 	
+	//NPDTP Done
 	public int FindNpdtp(String CodeLine) {
 		int Li_Count = 0;
+		Scanner scanner = new Scanner(CodeLine);
+		while (scanner.hasNext()) {
+			token1 = scanner.next();
 			
+			if((token1.contains("public")) || (token1.contains("private"))) {
+				
+				token1 = scanner.next();
+				
+				while (!token1.contains(")")) {
+					
+					if ((token1.contains("int")) || (token1.contains("long")) || (token1.contains("double"))
+							|| (token1.contains("float")) || (token1.contains("boolean")) || (token1.contains("char"))) {
+						Li_Count = Li_Count + 1;
+					}
+										
+					token1 = scanner.next();
+				}
+				
+			}
+			
+			Li_Count = 0;
+			NPDTPM = Li_Count;
+			return 0;					
+		}
+		scanner.close();
+		NPDTPM = Li_Count;
 		return Li_Count;
 	}
 	
+	//NCDTP Done
 	public int FindNcdtp(String CodeLine) {
 		int Li_Count = 0;
-		
+		String token1 = "";
+		Scanner scanner = new Scanner(CodeLine);
+		while (scanner.hasNext()) {
+			token1 = scanner.next();
+			
+			if((token1.contains("public")) || (token1.contains("private"))) {
+				
+				token1 = scanner.next();
+				
+				while (!token1.contains(")")) {
+					
+					if ((token1.contains("arrays")) || (token1.contains("objects")) || (token1.contains("records"))
+							|| (token1.contains("interfaces")) || (token1.contains("lists")) || (token1.contains("String[]")) || (token1.contains("String"))) {
+						Li_Count = Li_Count + 1;
+					
+					}					
+					token1 = scanner.next();
+				}	
+			}
+		}
+		scanner.close();
+		NCDTPM = Li_Count;
 		return Li_Count;
 	}
 	
+	//CM Done
 	public int FindCm(String CodeLine) {
-		int Li_Count = 0;
-		
-		return Li_Count;
+		sum = (NCDTPM * 2) + (NPDTPM * 1);
+		return sum;
 	}
 }
