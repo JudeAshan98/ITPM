04217e16620fc1fdad4c660e4e6c1fd01749a411
maaliWageesha <malidi26@yahoo.com>
2020-04-08 20:47:31 +0530

    Cpp added to inheritance

---
+++ CDE_IT/src/ValueGenerator/InheritData.java
@@ -0,0 +1,163 @@
+package ValueGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import xmlReader.*;
+
+public class InheritData {
+	
+
+	String Result;
+	String token1 = "\n";
+	private int Li_zero;
+	private int Li_one;
+	private int Li_two;
+	private int Li_three;
+	private int Li_four;
+//	
+//	
+	List<Integer> ValueList = new ArrayList<Integer>();
+	inheritanceReader inheritanceReader = new inheritanceReader();
+	String previousLine = null;
+//	
+	public int getLi_zero() {
+		return Li_zero;
+	}
+	public void setLi_zero(int li_DirIn) {
+		ValueList = inheritanceReader.GetAllcsValues();
+		Li_zero = ValueList.get(0);
+	}
+
+	public int getLi_one() {
+		return Li_one;
+	}
+	public void setLi_one(int li_one) {
+		ValueList = inheritanceReader.GetAllcsValues();
+		Li_one = ValueList.get(1);
+	}
+	public int getLi_two() {
+		return Li_two;
+	}
+	public void setLi_two(int li_two) {
+		ValueList = inheritanceReader.GetAllcsValues();
+		Li_two = ValueList.get(2);
+	}
+	public int getLi_three() {
+		return Li_three;
+	}
+	public void setLi_three(int li_three) {
+		ValueList = inheritanceReader.GetAllcsValues();
+		Li_three = ValueList.get(3);
+	}
+	public int getLi_four() {
+		return Li_four;
+	}
+	public void setLi_four(int li_four) {
+		ValueList = inheritanceReader.GetAllcsValues();
+		Li_four = ValueList.get(4);
+	}
+	
+	
+	// methods
+	
+	public int DirectInh(String CodeLine) {
+		String s1="";
+		String s2="";
+		String s3="";
+		String s4="";
+		String s5="";
+	
+		int Li_one_count= 0;
+		Scanner scanner = new Scanner(CodeLine);
+		while (scanner.hasNext()) {
+			token1 = scanner.next();
+			if (token1.contains("extends")) {
+				
+				s1=scanner.next();
+				System.out.println(s1);
+				s2=scanner.next();
+				
+				if(s2.equals("{")) {
+					Li_one_count=Li_one_count+1;
+					return Li_one_count;
+				}	
+				else {
+					s3=scanner.next();
+					if(s3.equals("{"))
+					{
+						Li_one_count=Li_one_count+2;
+						return Li_one_count;
+						
+					}else {
+						s4=scanner.next();
+						if(s4.equals("{")) {
+							
+							Li_one_count=Li_one_count+3;
+							return Li_one_count;
+						}else {
+							s5=scanner.next();
+							if(s5.equals("{"))
+							{
+								Li_one_count=Li_one_count+4;
+								return Li_one_count;
+							}else {
+								
+								Li_one_count=Li_one_count+5;
+								return Li_one_count;
+							}
+							
+						}
+						
+					}
+				}
+				
+
+			}
+		}
+		
+		scanner.close();
+		System.out.println(Li_one_count);
+		return Li_one_count;
+	}
+	
+	
+	public int InDirectInh(String CodeLine) {
+		
+		int Li_two_count= 0;
+		return Li_two_count;
+	}
+	
+	
+	public int totalWeight(int Three)
+	{
+		int count=0;
+		
+		if(Three==0)
+		{
+			setLi_zero(Li_zero);
+			count = count + getLi_zero();
+		}else if(Three==1)
+		{
+			setLi_one(Li_one);
+			count = count + getLi_one();
+		}else if(Three==2)
+		{
+			setLi_two(Li_two);
+			count = count + getLi_two();
+		}else if(Three==3)
+		{
+			setLi_three(Li_three);
+			count = count + getLi_three();
+		}else {
+			
+			setLi_four(Li_four);
+			count = count + getLi_four();
+		}
+		
+		
+		return count;
+	}
+
+}
---
+++ CDE_IT/src/xmlReader/inheritanceReader.java
@@ -0,0 +1,71 @@
+package xmlReader;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class inheritanceReader {
+	
+      int Li_zero;
+	  int Li_one;
+	  int Li_two;
+	  int Li_three;
+	  int Li_four;
+	  List <Integer>XmlValues = new ArrayList<Integer>();
+	  // This method can read values from the xml and return data to the valuegenerator class.
+	public List<Integer> GetAllcsValues(){
+		  
+  try {
+
+	File fXmlFile = new File("C:\\Users\\Malidi\\git\\ITPM\\CDE_IT\\WebContent\\xml\\inheritParser.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+	
+	
+	
+	doc.getDocumentElement().normalize();
+
+	//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			
+	NodeList nList = doc.getElementsByTagName("inhvariables");
+
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+
+		Node nNode = nList.item(temp);
+		
+	//	System.out.println("\nCurrent Element :" + nNode.getNodeName());
+				
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+			
+			Element eElement = (Element) nNode;
+			
+			Li_zero = Integer.parseInt((eElement.getElementsByTagName("zeroU").item(0).getTextContent()));
+			XmlValues.add(Li_zero);
+			Li_one = Integer.parseInt((eElement.getElementsByTagName("oneU").item(0).getTextContent()));
+			XmlValues.add(Li_one);
+			Li_two = Integer.parseInt(eElement.getElementsByTagName("twoU").item(0).getTextContent());
+			XmlValues.add(Li_two);
+			Li_three =Integer.parseInt(eElement.getElementsByTagName("threeU").item(0).getTextContent());
+			XmlValues.add(Li_three);
+			Li_four =Integer.parseInt(eElement.getElementsByTagName("fourU").item(0).getTextContent());
+			XmlValues.add(Li_four);
+		}
+	}
+  } catch (Exception e) {
+	e.printStackTrace();
+  }
+	return XmlValues;
+  
+	 }
+
+
+}
