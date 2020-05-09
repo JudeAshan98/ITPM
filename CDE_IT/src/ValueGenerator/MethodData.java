6440b4c701afb688e882ec6e127b3e6014884f9a
GIT18195644 <it18195644@my.sliit.lk>
2020-05-09 16:02:32 +0530

    SVM C++ Completed

--- CDE_IT/src/ValueGenerator/MethodData.java
+++ CDE_IT/src/ValueGenerator/MethodData.java
@@ -97,11 +97,30 @@ public class MethodData {
 			while (scanner.hasNext()) {
 				token1 = scanner.next();
 
-				if(token1.startsWith("#") || token1.startsWith("//") || token1.startsWith("/*")) {
+				if (token1.startsWith("#") || token1.startsWith("//") || token1.startsWith("/*")) {
 					scanner.close();
 					return a = 0;
 				}
-			
+
+				if ((token1.contains("int")) || (token1.contains("long")) || (token1.contains("double"))
+						|| (token1.contains("float")) || (token1.contains("boolean")) || (token1.contains("char"))
+						|| (token1.contains("arrays")) || (token1.contains("objects")) || (token1.contains("records"))
+						|| (token1.contains("interfaces")) || (token1.contains("lists"))
+						|| (token1.contains("String"))) {
+
+					if (scanner.hasNext()) {
+						token1 = scanner.next();
+					}
+
+					if (token1.endsWith("()")) {
+						Li_Count = Li_Count + 2;
+					} else if (token1.endsWith("(")) {
+						Li_Count = Li_Count + 1;
+					} else {
+						scanner.close();
+						return a = 0;
+					}
+				}
 			}
 		}
 
@@ -143,7 +162,33 @@ public class MethodData {
 				}
 			}
 		} else if (extention.equals("cpp")) {
+			token1 = "";
+			while (scanner.hasNext()) {
+				token1 = scanner.next();
 
+				if (token1.startsWith("#") || token1.startsWith("//") || token1.startsWith("/*")) {
+					scanner.close();
+					return b = 0;
+				}
+
+				if ((token1.contains("int")) || (token1.contains("float")) || (token1.contains("bool"))
+						|| (token1.contains("char")) || (token1.contains("double")) || (token1.contains("void"))
+						|| (token1.contains("wchar_t"))) {
+
+					if (scanner.hasNext()) {
+						token1 = scanner.next();
+					}
+
+					if (token1.endsWith("()")) {
+						Li_Count = Li_Count + 1;
+					} else if (token1.endsWith("(")) {
+						Li_Count = Li_Count + 1;
+					} else {
+						scanner.close();
+						return b = 0;
+					}
+				}
+			}
 		}
 
 		scanner.close();
@@ -200,7 +245,33 @@ public class MethodData {
 				}
 			}
 		} else if (extention.equals("cpp")) {
+			token1 = "";
+			while (scanner.hasNext()) {
+				token1 = scanner.next();
 
+				if (token1.startsWith("#") || token1.startsWith("//") || token1.startsWith("/*")) {
+					scanner.close();
+					return c = 0;
+				}
+
+				if ((token1.equals("int[]")) || (token1.equals("float[]")) || (token1.equals("bool[]"))
+						|| (token1.equals("char[]")) || (token1.equals("double[]")) || (token1.equals("void[]"))
+						|| (token1.equals("wchar_t[]"))) {
+
+					if (scanner.hasNext()) {
+						token1 = scanner.next();
+					}
+
+					if (token1.endsWith("()")) {
+						Li_Count = Li_Count + 1;
+					} else if (token1.endsWith("(")) {
+						Li_Count = Li_Count + 1;
+					} else {
+						scanner.close();
+						return c = 0;
+					}
+				}
+			}
 		}
 
 		scanner.close();
